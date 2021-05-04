0240c33615d3fee75346b15d2888dc6cebebf90e
MichaeLaudrup <maikelaudrupluisgonzalez@gmail.com>
2021-05-04 17:55:22 +0100

    finalizada parte 1 del curso

--- Ejercicios JavaScript/js/scriptAssignments.js
+++ Ejercicios JavaScript/js/scriptAssignments.js
@@ -1,6 +1,7 @@
 
 //ASSIGNEMENTS JAVASCRIPT COURSE FROM ZERO TO EXPERT UDEMY 
-
+const numMaxLecturas = 50; 
+const lecturesStatements = new Array(numMaxLecturas); 
 
 /*LECTURE: VALUES AND VARIABLES
 1.Declare variables called 'country', 'continent'and 'population'and assign their values according to your own country(population in millions)
@@ -11,7 +12,7 @@ let continent = "Europe";
 let population = 45000000; 
 
 let textToPrint = `In the continent ${continent} there are a country called ${country}, which have a population of ${population} in 2021`; 
-console.log(textToPrint); 
+
 
 /*LECTURE: DATA TYPES 
 1. Declare a variable called 'isIsland' and set its value according to your 
@@ -24,7 +25,7 @@ to the console
 let isIsland = false; 
 let language; 
 textToPrint = `Type of variable "country" is ${typeof country} \nType of variable "population" is ${typeof population}\nType of variable "isIsland" is ${typeof isIsland}\nType of variable "language" is ${typeof language}`;   
-console.log(textToPrint); 
+
 
 /*LECTURE let, const and var 
 1. Set the value of 'language' to the language spoken where you live (some 
@@ -53,12 +54,11 @@ const yearOfBirth = 1994;
 */
 const populationFinland = 6*(10**6); 
 const populationAverage = 33000000; 
-console.log("Population/2 --> " + population/2);
+/* console.log("Population/2 --> " + population/2);
 console.log("Population++ --> " + population++); 
 console.log("Does your country have more people than Finland? --> "+ (population > populationFinland )) ; 
-console.log("Does your country have less people than the average country? -->" + (population < populationAverage)); 
+console.log("Does your country have less people than the average country? -->" + (population < populationAverage));  */
 textToPrint = country + " is in " + continent + ",and its " + (population/(10**6)).toFixed() + " million people speak " + language; 
-console.log(textToPrint ); 
 
 /*LECTURE: Strings and Template Literals 
     1. Recreate the 'description' variable from the last assignment, this time 
@@ -74,7 +74,7 @@ textToPrint = `${country} is in ${continent},and its ${(population/(10**6)).toFi
     2. After checking the result, change the population temporarily to 13 and then to 
     130. See the different results, and set the population back to original
 */
-population = 11000000
+
 let result; 
 if(population > 33*(10**6)){
     result = 'above'; 
@@ -82,10 +82,113 @@ if(population > 33*(10**6)){
     result = ((33*(10**6) - population)/(10**6)).toFixed()+' million below'
 }
 textToPrint = `${country}'s population is ${result} average`; 
-console.log(textToPrint); 
-
-/*
 
 
+/*LECTURE: Type Conversion and Coercion */ 
+lecturesStatements[6] =
+`1. Predict the result of these 5 operations without executing them: 
+    '9' - '5'; --> 4
+    '19' - '13' + '17'; --> 6 + '17' --> '617'
+    '19' - '13' + 17; --> 6 + 17 --> 23
+    '123' < 57; --> false
+    5 + 6 + '4' + 9 - 4 - 2; '1149' - 6 = 1143
+2. Execute the operations to check if you were right`; 
 
-*/ 
+/* console.log("El resultado de la operacion '9'-'5' es: " + ('9' - '5') + ' el cual es de tipo ' + typeof('9' - '5'));
+console.log("El resultado de la operacion '19' - '13' + '17' es: " + ('19' - '13' + '17') + ' el cual es de tipo ' + typeof('19' - '13' + '17'));
+console.log("El resultado de la operacion '19' - '13' + 17 es: " + ('19' - '13' + 17) + ' el cual es de tipo ' + typeof('19' - '13' + 17));
+console.log("El resultado de la operacion '123' < 57 es: " + ('123' < 57) + ' el cual es de tipo ' + typeof('123' < 57));
+console.log("El resultado de la operacion 5 + 6 + '4' + 9 - 4 - 2 es: " + (5 + 6 + '4' + 9 - 4 - 2) + ' el cual es de tipo ' + typeof(5 + 6 + '4' + 9 - 4 - 2)); */
+
+
+/*LECTURE: Equality Operators: == vs. ===*/
+lecturesStatements[7] = 
+`1. Declare a variable 'numNeighbours' based on a prompt input like this: 
+    prompt('How many neighbour countries does your country 
+    have?'); 
+2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality == for now) 
+3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours' is greater than 1 
+4. Use an else block to log 'No borders' (this block will be executed when 'numNeighbours' is 0 or any other value) 
+5. Test the code with different values of 'numNeighbours', including 1 and 0. 
+6. Change == to ===, and test the code again, with the same values of 
+   'numNeighbours'. Notice what happens when there is exactly 1 border! Why 
+   is this happening? 
+7. Finally, convert 'numNeighbours' to a number, and watch what happens now 
+   when you input 1 
+8. Reflect on why we should use the === operator and type conversion in this situation `; 
+
+/* RESOLUTION */
+/* let numNeighbours = Number(prompt('How many neighbour countries does your country have?')); 
+
+if(numNeighbours === 1){
+    console.log('Only 1 border!');
+}else if(numNeighbours > 1){
+    console.log('More than 1 border');
+}else{
+    console.log('No borders');
+} */
+
+
+/* LECTURE: Logical Operators*/
+lecturesStatements[8] = 
+`1.  Comment out the previous code so the prompt doesn't get in the way 
+2.  Let's say Sarah is looking for a new country to live in. She wants to live in a 
+    country that speaks english, has less than 50 million people and is not an 
+    island. 
+3.  Write an if statement to help Sarah figure out if your country is right for her. 
+    You will need to write a condition that accounts for all of Sarah's criteria. Take 
+    your time with this, and check part of the solution if necessary. 
+4.  If yours is the right country, log a string like this: 'You should live in Portugal :)'. If 
+    not, log 'Portugal does not meet your criteria :(' 
+5.  Probably your country does not meet all the criteria. So go back and temporarily 
+    change some variables in order to make the condition true (unless you live in 
+    Canada :D) ` ; 
+
+/* SOLUTION */
+/* if((language==='English') && (population < 50*(10**6)) && !isIsland  ){
+    textToPrint = `You should live in ${country} :)`; 
+}else{
+    textToPrint = `${country} does not meet your criteria :(`; 
+}
+console.log(textToPrint);  */
+
+/*LECTURE: The switch Statement*/
+lecturesStatements[8] =
+`1. Use a switch statement to log the following string for the given 'language': 
+    chinese or mandarin: 'MOST number of native speakers!' 
+    spanish: '2nd place in number of native speakers' 
+    english: '3rd place' 
+    hindi: 'Number 4' 
+    arabic: '5th most spoken language' 
+    for all other simply log 'Great language too :D'`; 
+
+switch(language){
+    case 'chinese':
+    case 'mandarin':
+        textToPrint = "MOST number of native speakers!";
+        break; 
+    case 'spanish': 
+        textToPrint = "2nd place in number of native speakers!"; 
+        break; 
+    case 'english': 
+        textToPrint = "3rd place"; 
+        break; 
+    case 'hindi':
+        textToPrint = "Number 4"; 
+        break; 
+    case 'arabic': 
+        textToPrint= '5th most spoken language'; 
+        break; 
+    default: 
+        textToPrint = "Great language too"; 
+}
+/* /* LECTURE: The Conditional (Ternary) Operator */ 
+lecturesStatements[8] =
+`1. If your country's population is greater than 33 million, use the ternary operator 
+    to log a string like this to the console: 'Portugal's population is above average'. 
+    Otherwise, simply log 'Portugal's population is below average'. Notice how only 
+    one word changes between these two sentences! 
+    2. After checking the result, change the population temporarily to 13 and then to 
+    130. See the different results, and set the population back to original`;
+
+textToPrint = `${country}'s population is ${(population > 33) ? 'above' : 'below'} average`; 
--- Ejercicios JavaScript/js/scriptCodingChallenges.js
+++ Ejercicios JavaScript/js/scriptCodingChallenges.js
@@ -1,12 +1,14 @@
 
 //CODING CHALLENGES JAVASCRIPT COURSE FROM ZERO TO EXPERT UDEMY (2021)
-console.log("====================JAVACRIPT FROM ZERO TO EXPERT: CODING CHALLENGES ====================");
-/* /*CODING CHALLENGE NUMBER ONE */
+/* console.log("====================JAVACRIPT FROM ZERO TO EXPERT: CODING CHALLENGES(CC) ===================="); */
+/* /*CODING CHALLENGE #1 */
 const numMaxOfChallenges = 60; 
 const codingChallenges = new Array(numMaxOfChallenges);
-codingChallenges[0] = `Mark and John are trying to compare their BMI (Body Mass Index), which is 
-calculated using the formula: BMI = mass / height ** 2 = mass / (height * height) (mass in and height in meter). 
-Your tasks: 
+codingChallenges[0] = 
+`Mark and John are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula: BMI = mass / height ** 2 = mass / (height * height) (mass in and 
+height in meter). Your tasks: 
+
     1. Store Mark's and John's mass and height in variables 
     2. Calculate both their BMIs using the formula (you can even implement both 
     versions) 
@@ -16,9 +18,85 @@ Your tasks:
 const markBMI = (78 / 1.79 ** 2); 
 const johnBMI = (100 / (1.90 * 1.90)); 
 let markHigherBMI = (markBMI > johnBMI); 
-textToPrint = `The mark BMI is: ${markBMI.toFixed(2)}
+textToPrint = `-CC#1Result-\nThe mark BMI is: ${markBMI.toFixed(2)}
 The John BMI is: ${johnBMI.toFixed(2)}
 is Mark BMI higher than John BMI?: ${markHigherBMI}`; 
-console.log(textToPrint); 
 
-CODING
+
+/* CODING CHAGENGE #2 */
+codingChallenges[1] =
+`Use the BMI example from Challenge #1, and the code you already wrote, and 
+improve it. Your tasks: 
+    1. Print a nice output to the console, saying who has the higher BMI. The message 
+    is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!" 
+    2. Use a template literal to include the BMI values in the outputs. Example: "Mark's 
+    BMI (28.3) is higher than John's (23.9)!" `; 
+
+
+if(markHigherBMI){  //NOTE: Teacher says that is better put comparison with conditional's operators. 
+    textToPrint = `-CC#2Result-\nMark's BMI (${markBMI.toFixed(2)}) is higher that John's BMI (${johnBMI.toFixed(2)})`; 
+}else{
+    textToPrint = `-CC#2Result-\nJohn's BMI (${johnBMI.toFixed(2)}) is higher that Mark's BMI (${markBMI.toFixed(2)})`;  
+}
+
+/* CODING CHAGENGE #3 */
+codingChallenges[2] = 
+`There are two gymnastics teams, Dolphins and Koalas. They compete against each 
+other 3 times. The winner with the highest average score wins a trophy! 
+Your tasks: 
+    1. Calculate the average score for each team, using the test data below 
+    2. Compare the team's average scores to determine the winner of the competition, 
+       and print it to the console. Don't forget that there can be a draw, so test for that 
+       as well (draw means they have the same average score) 
+    3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a 
+       team only wins if it has a higher score than the other team, and the same time a 
+       score of at least 100 points. Hint: Use a logical operator to test for minimum 
+       score, as well as multiple else-if blocks 😉 
+    4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when 
+       both teams have the same score and both have a score greater or equal 100 
+       points. Otherwise, no team wins the trophy 
+Test data: 
+    * Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110 
+    * Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123 
+    * Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106 `;
+
+/* const dolphinAverage = (97+112+101)/3; 
+const koalaAverage = (109+95+106)/3; 
+textToPrint = `Dolphin average: ${dolphinAverage.toFixed(2)}; 
+Koala average: ${koalaAverage.toFixed(2)}`; 
+console.log(textToPrint); 
+if((dolphinAverage > koalaAverage) && (dolphinAverage >= 100)){
+    console.log( "dolphin wins!");
+}else if((koalaAverage > dolphinAverage) && (koalaAverage >= 100)){
+    console.log( "koala wins!");
+}else if((koalaAverage === dolphinAverage) && (koalaAverage >= 100) && (dolphinAverage >= 100) ){
+    console.log( "Both win the trophy!");
+}else{
+    console.log("LOSERS"); 
+} */
+
+/* Coding Challenge #4  */
+codingChallenges[3] =
+`Steven wants to build a very simple tip calculator for whenever he goes eating in a 
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 
+300. If the value is different, the tip is 20%. Your tasks: 
+    1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for 
+    this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can 
+    start with an if/else statement, and then try to convert it to a ternary 
+    operator!) 
+    2. Print a string to the console containing the bill value, the tip, and the final value 
+    (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 
+    316.25” 
+    Test data: 
+        § Data 1: Test for bill values 275, 40 and 430 
+        Hints: 
+        § To calculate 20% of a value, simply multiply it by 20/100 = 0.2 
+        § Value X is between 50 and 300, if it's >= 50 && <= 300 😉 `;
+
+const bill = 275; 
+const tip = (bill >= 50 && bill <= 300) ? 0.15*bill : 0.20 * bill; 
+textToPrint = `The bill was ${bill}, the tip was ${tip}, and the total value is ${bill+tip}`; 
+
+//***************************************************************************************************//
+//=================================PART 2 OF THE COURSE =============================================//
+//***************************************************************************************************//
