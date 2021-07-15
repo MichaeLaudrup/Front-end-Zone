9d37ec5051f2d67199fb48e293e068cc912c9f05
MichaeLaudrup <maikelaudrupluisgonzalez@gmail.com>
2021-07-15 20:10:02 +0100

    Project 5 finished

--- JavaScriptCourse_FromZeroToExpert/codingChallenges/scriptCodingChallenges.js
+++ JavaScriptCourse_FromZeroToExpert/codingChallenges/scriptCodingChallenges.js
@@ -868,10 +868,10 @@ codingChallenges[19] = `
   dogs.forEach(
     dog => (dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28))
   ); //Add recommend ration to dog objects
-  console.log(dogs);
+  //console.log(dogs);
 
   const dogOfSarah = dogs.find(dog => dog.owners.includes('Sarah'));
-  console.log(dogOfSarah);
+  //console.log(dogOfSarah);
 
   const { ownersEatTooMuch, ownersEatTooLittle } = dogs.reduce(
     (acc, dog) => {
@@ -887,7 +887,7 @@ codingChallenges[19] = `
     },
     { ownersEatTooMuch: [], ownersEatTooLittle: [] }
   );
-  console.log(ownersEatTooMuch, ownersEatTooLittle);
+  //console.log(ownersEatTooMuch, ownersEatTooLittle);
 
   const phrase = ownersEatTooLittle
     .flat()
@@ -907,7 +907,7 @@ codingChallenges[19] = `
         (index !== arr.length - 1 ? ' and ' : "'s dogs eat too much. "),
       ''
     );
-  console.log(phrase, phrase2);
+  /* console.log(phrase, phrase2);
 
   console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
   console.log(
@@ -916,30 +916,245 @@ codingChallenges[19] = `
         dog.curFood < dog.recommendedFood * 1.1 &&
         dog.curFood > dog.recommendedFood * 0.9
     )
-  );
+  ); */
 
   const dogsEatCorrectly = dogs.filter(
     dog =>
       dog.curFood < dog.recommendedFood * 1.1 &&
       dog.curFood > dog.recommendedFood * 0.9
   );
-  console.log(dogsEatCorrectly);
+  //console.log(dogsEatCorrectly);
 
   const dogsCopy = dogs.slice().sort((a, b) => a.curFood - b.curFood);
-  console.log(dogsCopy);
+  //console.log(dogsCopy);
 } //END OF CODING CHALLENGE NUMBER 19
 
 //CODING CHAGENGE 20
-codingChallenges[20] = ``;
+codingChallenges[20] = `
+  1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 
+  'speed' property. The 'speed' property is the current speed of the car in 
+  km/h 
+  2. Implement an 'accelerate' method that will increase the car's speed by 10, 
+  and log the new speed to the console 
+  3. Implement a 'brake' method that will decrease the car's speed by 5, and log 
+  the new speed to the console 
+  4. Create 2 'Car' objects and experiment with calling 'accelerate' and 
+  'brake' multiple times on each of them 
+  Test data: 
+    § Data car 1: 'BMW' going at 120 km/h 
+    § Data car 2: 'Mercedes' going at 95 km/h `;
 {
+  const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+  };
+
+  Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(this.speed);
+  };
+
+  Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(this.speed);
+  };
+  const car1 = new Car('BMW', 120);
+  const car2 = new Car('Mercerdes', 95);
 } //END OF CODING CHALLENGE NUMBER 20
 
 //CODING CHAGENGE 21
-codingChallenges[21] = ``;
+codingChallenges[21] = `
+Coding Challenge #2 
+Your tasks: 
+1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl') 
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide 
+by 1.6) 
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but 
+converts it to km/h before storing the value, by multiplying the input by 1.6) 
+4. Create a new car and experiment with the 'accelerate' and 'brake' 
+methods, and with the getter and setter. 
+Test data: 
+§ Data car 1: 'Ford' going at 120 km/h `;
 {
+  class CarCl {
+    constructor(make, speed) {
+      this.make = make;
+      this.speed = speed;
+    }
+    accelerate() {
+      this.speed += 10;
+      console.log(`${this.make} going to ${this.speed}`);
+    }
+    brake() {
+      this.speed -= 5;
+      console.log(`${this.make} going to ${this.speed}`);
+    }
+    get speedUS() {
+      return this.speed / 1.6;
+    }
+    set speedUS(speed) {
+      this.speed = speed * 1.6;
+    }
+  }
+
+  const car1 = new CarCl('ibiza', 120);
+  console.log(car1.speedUS);
+  car1.accelerate();
+
+  console.log(car1);
 } //END OF CODING CHALLENGE NUMBER 21
 
 //CODING CHAGENGE 22
-codingChallenges[22] = ``;
+codingChallenges[22] = `
+Coding Challenge #3 
+  Your tasks: 
+  1. Use a constructor function to implement an Electric Car (called 'EV') as a child 
+  "class" of 'Car'. Besides a make and current speed, the 'EV' also has the 
+  current battery charge in % ('charge' property) 
+  2. Implement a 'chargeBattery' method which takes an argument 
+  'chargeTo' and sets the battery charge to 'chargeTo' 
+  3. Implement an 'accelerate' method that will increase the car's speed by 20, 
+  and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 
+  km/h, with a charge of 22%' 
+  4. Create an electric car object and experiment with calling 'accelerate', 
+  'brake' and 'chargeBattery' (charge to 90%). Notice what happens when 
+  you 'accelerate'! Hint: Review the definiton of polymorphism 😉 
+  Test data: 
+  § Data car 1: 'Tesla' going at 120 km/h, with a charge of 23% 
+
+ 
+
+GOOD LUCK 😀 `;
 {
+  const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+  };
+
+  Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} going to ${this.speed}`);
+  };
+
+  Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} going to ${this.speed}`);
+  };
+
+  const EV = function (make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+  };
+
+  EV.prototype = Object.create(Car.prototype);
+
+  EV.prototype.chargeTo = function (charge) {
+    this.charge = charge;
+  };
+  EV.prototype.accelerate = function () {
+    this.speed += 20;
+    this.charge = this.charge * 0.99;
+    console.log(
+      `${this.make} going to ${
+        this.speed
+      }, with a charge of ${this.charge.toFixed(2)}%`
+    );
+  };
+
+  const electricCar = new EV('Ibiza', 120, 100);
+  electricCar.accelerate();
+  electricCar.accelerate();
+  electricCar.accelerate();
+  electricCar.brake();
 } //END OF CODING CHALLENGE NUMBER 22
+
+//CODING CHAGENGE 23
+codingChallenges[23] = `Coding Challenge #4 
+Your tasks: 
+1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl' 
+child class of the 'CarCl' class 
+2. Make the 'charge' property private 
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' 
+methods of this class, and also update the 'brake' method in the 'CarCl' 
+class. Then experiment with chaining! 
+Test data: 
+§ Data car 1: 'Rivian' going at 120 km/h, with a charge of 23% 
+
+ 
+
+GOOD LUCK 😀 `;
+{
+  const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+  };
+
+  Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} going to ${this.speed}`);
+  };
+
+  Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} going to ${this.speed}`);
+    return this;
+  };
+
+  class EVCl extends Car {
+    #charge;
+    constructor(make, speed, charge) {
+      super(make, speed);
+      this.#charge = charge;
+    }
+
+    accelerate() {
+      this.speed += 20;
+      this.#charge = this.#charge - 1;
+      console.log(
+        `${this.make} going to ${
+          this.speed
+        }, with a charge of ${this.#charge.toFixed(2)}%`
+      );
+      return this;
+    }
+    chargeTo(charge) {
+      this.charge = charge;
+      return this;
+    }
+  }
+
+  const electricCar = new EVCl('BMW', 120, 56);
+  electricCar
+    .accelerate()
+    .brake()
+    .brake()
+    .accelerate()
+    .accelerate()
+    .accelerate()
+    .accelerate();
+} //END OF CODING CHALLENGE NUMBER 23
+
+//CODING CHAGENGE 24
+codingChallenges[24] = ``;
+{
+} //END OF CODING CHALLENGE NUMBER 24
+
+//CODING CHAGENGE 24
+codingChallenges[24] = ``;
+{
+} //END OF CODING CHALLENGE NUMBER 24
+
+//CODING CHAGENGE 24
+codingChallenges[24] = ``;
+{
+} //END OF CODING CHALLENGE NUMBER 24
+
+//CODING CHAGENGE 24
+codingChallenges[24] = ``;
+{
+} //END OF CODING CHALLENGE NUMBER 24
+
+//CODING CHAGENGE 24
+codingChallenges[24] = ``;
+{
+} //END OF CODING CHALLENGE NUMBER 24
